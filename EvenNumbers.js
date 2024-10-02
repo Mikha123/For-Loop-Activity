b49767c389050ccd5308f59976681f476c37a835
Mikhaella Nadonga <soriaomikhaella@gmail.com>
2024-10-02 11:19:45 +0800

    Add files via upload

---
+++ EvenNumbers.js
@@ -0,0 +1,7 @@
+//Write a program that prints numbers 1 to 10 using a for loop.
+//Modify the program to print only even numbers between 1 and 10.
+// Log the numbers to the console.
+
+for (let i=0; i<=10; i+=2) {
+  console.log(i);
+}
